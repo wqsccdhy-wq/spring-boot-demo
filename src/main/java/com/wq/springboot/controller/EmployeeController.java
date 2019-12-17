be13745f03e375dd78267a58702fb6e95503a89a
wqsccdhy-wq <wangqingsc2009@126.com>
2019-12-17 22:01:46 +0800

    抽取公共页面

---
+++ src/main/java/com/wq/springboot/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.wq.springboot.controller;
+
+import com.wq.springboot.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.wq.springboot.dao.EmployeeDao;
+
+import java.util.Collection;
+
+/**
+ * @author wangqing
+ * @Desc
+ * @Date: 2019-12-17 20:34
+ * @since 2019/12/17 20:34
+ */
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+
+    @GetMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> all = employeeDao.getAll();
+        System.out.println(all.size());
+        return "emp/list";
+    }
+}
