d625256ffbf869a93b7db76283eabf90d46988d2
wqsccdhy-wq <wangqingsc2009@126.com>
2019-12-18 21:09:49 +0800

    修改员工

--- src/main/java/com/wq/springboot/controller/EmployeeController.java
+++ src/main/java/com/wq/springboot/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -27,6 +29,11 @@ public class EmployeeController {
     @Autowired
     private DepartmentDao departmentDao;
 
+    /**
+     * 获取所有员工
+     * @param model
+     * @return
+     */
     @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> all = employeeDao.getAll();
@@ -34,6 +41,11 @@ public class EmployeeController {
         return "emp/list";
     }
 
+    /**
+     * 添加员工页面
+     * @param model
+     * @return
+     */
     @GetMapping("/emp")
     public String addPage(Model model) {
         Collection<Department> departments = departmentDao.getDepartments();
@@ -41,12 +53,38 @@ public class EmployeeController {
         return "emp/add";
     }
 
-//    @PostMapping("/emp")
-//    public String addEmp(Employee employee){
-//        employeeDao.save(employee);
-//        return "forward:/emps";
-//    }
+    /**
+     * 修改员工页面
+     * @param model
+     * @param id
+     * @return
+     */
+    @GetMapping("/emp/{id}")
+    public String toEditPage(Model model,@PathVariable("id") Integer id){
+        //employeeDao.save(employee);
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("deps", departments);
+        return "emp/add";
+    }
 
+    /**
+     * 更新员工
+     * @param employee
+     * @return
+     */
+    @PutMapping("/emp")
+    public String updateEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    /**
+     * 添加员工
+     * @param employee
+     * @return
+     */
     @PostMapping("/emp")
     public String addEmp(Employee employee){
         //来到员工列表页面
