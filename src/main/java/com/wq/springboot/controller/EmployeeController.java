3e559e3577b905782128407b827526d561419a30
wqsccdhy-wq <wangqingsc2009@126.com>
2019-12-18 18:00:44 +0800

    添加员工

--- src/main/java/com/wq/springboot/controller/EmployeeController.java
+++ src/main/java/com/wq/springboot/controller/EmployeeController.java
@@ -1,12 +1,14 @@
 package com.wq.springboot.controller;
 
+import com.wq.springboot.dao.DepartmentDao;
+import com.wq.springboot.dao.EmployeeDao;
+import com.wq.springboot.entities.Department;
 import com.wq.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.wq.springboot.dao.EmployeeDao;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -22,10 +24,36 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> all = employeeDao.getAll();
         model.addAttribute("emps", all);
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String addPage(Model model) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("deps", departments);
+        return "emp/add";
+    }
+
+//    @PostMapping("/emp")
+//    public String addEmp(Employee employee){
+//        employeeDao.save(employee);
+//        return "forward:/emps";
+//    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //来到员工列表页面
+        //保存员工
+        employeeDao.save(employee);
+        // redirect: 表示重定向到一个地址  /代表当前项目路径
+        // forward: 表示转发到一个地址
+        return "redirect:/emps";
     }
 }
